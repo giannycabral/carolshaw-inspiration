b089ff945a74b124cd14443b6e805038116cdfb4
giannycabral <regiane.miranda@outlook.com.br>
2023-04-27 18:22:06 -0300

    mensagem

---
+++ assets/js/questions.js
@@ -0,0 +1,42 @@
+export default [
+    {
+      question: "Em que ano Carol desenvolveu seu primeiro jogo?",
+      answers: [
+        { option: "1978", correct: true },
+        { option: "1950", correct: false },
+        { option: "1995", correct: false },
+      ],
+    },
+    {
+      question: "Na Atari, qual foi um dos primeiro projetos que trabalhou?",
+      answers: [
+        { option: "3D Tic Tac Toe", correct: false },
+        { option: "Apolo", correct: true },
+        { option: "Super Breakout", correct: false },
+      ],
+    },
+    {
+      question: "Em que ano iniciou na Activision?",
+      answers: [
+        { option: "1990", correct: false },
+        { option: "1980", correct: false },
+        { option: "1982", correct: true },
+      ],
+    },
+    {
+      question: "Qual jogo criado por Carol que teve muito sucesso nos anos 80?",
+      answers: [
+        { option: "Calculator", correct: false },
+        { option: "Happy Trails", correct: false },
+        { option: "River Raid", correct: true },
+      ],
+    },
+    {
+      question: "Que ano Carol recebeu o Industry Icon Award, nos The Game Awards?",
+      answers: [
+        { option: "1990", correct: false },
+        { option: "2017", correct: true },
+        { option: "2000", correct: false },
+      ],
+    },
+];
---
+++ assets/js/script.js
@@ -0,0 +1,106 @@
+//---Slider de fotos
+
+const slider = document.querySelectorAll('.slider');
+const btnPrev = document.getElementById('prev-button');
+const btnNext = document.getElementById('next-button');
+
+let currentSlide = 0;
+
+function hideSlider() {
+  slider.forEach(item => item.classList.remove('on'))
+}
+
+function showSlider() {
+  slider[currentSlide].classList.add('on')
+}
+
+function nextSlider() {
+  hideSlider()
+  if(currentSlide === slider.length -1) {
+    currentSlide = 0
+  } else {
+    currentSlide++
+  }
+  showSlider()
+}
+
+function prevSlider() {
+  hideSlider()
+  if(currentSlide === 0) {
+    currentSlide = slider.length -1
+  } else {
+    currentSlide--
+  }
+  showSlider()
+}
+
+btnNext.addEventListener('click', nextSlider)
+btnPrev.addEventListener('click', prevSlider)
+
+//-----Quiz
+
+const question = document.querySelector(".question");
+const answers = document.querySelector(".answers");
+const spnQtd = document.querySelector(".spnQtd");
+const textFinish = document.querySelector(".finish span");
+const content = document.querySelector(".content");
+const contentFinish = document.querySelector(".finish");
+const btnRestart = document.querySelector(".finish button");
+
+import questions from './questions.js'
+
+let currentIndex = 0;
+let questionsCorrect = 0;
+
+btnRestart.onclick = () => {
+  content.style.display = "flex";
+  contentFinish.style.display = "none";
+
+  currentIndex = 0;
+  questionsCorrect = 0;
+  loadQuestion();
+};
+
+function nextQuestion(e) {
+  if (e.target.getAttribute("data-correct") === "true") {
+    questionsCorrect++;
+  }
+
+  if (currentIndex < questions.length - 1) {
+    currentIndex++;
+    loadQuestion();
+  } else {
+    finish();
+  }
+}
+
+function finish() {
+  textFinish.innerHTML = `você acertou ${questionsCorrect} de ${questions.length}`;
+  content.style.display = "none";
+  contentFinish.style.display = "flex";
+}
+
+function loadQuestion() {
+  spnQtd.innerHTML = `${currentIndex + 1}/${questions.length}`;
+  const item = questions[currentIndex];
+  answers.innerHTML = "";
+  question.innerHTML = item.question;
+
+  item.answers.forEach((answer) => {
+    const div = document.createElement("div");
+
+    div.innerHTML = `
+    <button class="answer" data-correct="${answer.correct}">
+      ${answer.option}
+    </button>
+    `;
+
+    answers.appendChild(div);
+  });
+
+  document.querySelectorAll(".answer").forEach((item) => {
+    item.addEventListener("click", nextQuestion);
+  });
+}
+
+loadQuestion();
